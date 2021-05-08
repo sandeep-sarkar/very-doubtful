97b85a2b7b5096b2a114fa0ebb7a85319ff428f6
Sandeep Sarkar <sandeep.sarkar@in.ibm.com>
2021-05-08 15:14:22 +0530

    Updated client code

---
+++ client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+
+	pb "github.com/very-doubtful/proto/calcstatisticsb"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	host = "localhost:50061"
+)
+
+func main() {
+	conn, err := grpc.Dial(host, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewStatisticsCalculatorClient(conn)
+
+	document, err := ioutil.ReadFile("test.csv")
+	if err != nil {
+		log.Fatal("Couldn't read input document")
+	}
+	ctx := context.Background()
+	resp, err := client.CalculateStatistics(ctx, &pb.CalculateStatisticsRequest{
+		Document: &pb.Document{
+			Content: document,
+		},
+	})
+
+	ioutil.WriteFile("out.csv", resp.GetContent(), 0644)
+}
